4ce0e3ff3b12f8bfdba2ee19c2a3b5fbb78c519e
manoelhns <mhns@cesar.school>
2026-09-15 19:55:38 -0300

    Add componente Input e Button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import styles from "./app.module.css"
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
+import { Input } from "./components/Input"
+import { Button } from "./components/Button"
 
 export default function() {
 
@@ -24,6 +26,13 @@ export default function() {
         <Letter value="A"/>
         <Letter value="C"/>
         <Letter value="T"/>
+      </div>
+
+      <h4>Palpite</h4>
+
+      <div className={styles.guess}>
+        <Input autoFocus maxLength={1} placeholder="?" />
+        <Button title="Confirmar"/>
       </div>
 
     </main>
