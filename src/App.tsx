c10e7faf008a64c28a8043fedc0a8c2a5ffb45a9
manoelhns <mhns@cesar.school>
2026-09-15 19:22:03 -0300

    Add componente Letter e estilização do main

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import styles from "./app.module.css"
 
 import { Header } from "./components/Header"
-import { Tip } from "./components/tip"
+import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 export default function() {
 
@@ -16,6 +17,15 @@ export default function() {
       <Header current={5} max={10} onRestart={handleRestartGame} />
       
       <Tip tip="Umas das linguagens de programação mais utilizadas "/>
+
+      <div className={styles.word}>
+        <Letter value="R"/>
+        <Letter value="E"/>
+        <Letter value="A"/>
+        <Letter value="C"/>
+        <Letter value="T"/>
+      </div>
+
     </main>
     
   </div>
